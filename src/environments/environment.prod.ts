15f1e8f2cc1e5677b8e9703843de16607fbfe195
Vadym Makevytss <vadymlat@mail.ru>
2020-10-20 13:28:42 +0300

    merge git

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyB0FmBwwKtiIWpYGMcXwr7TUTBdcJKFkCE",
+    authDomain: "fitness-traker-d4210.firebaseapp.com",
+    databaseURL: "https://fitness-traker-d4210.firebaseio.com",
+    projectId: "fitness-traker-d4210",
+    storageBucket: "fitness-traker-d4210.appspot.com",
+    messagingSenderId: "108275168631",
+    appId: "1:108275168631:web:f4344a7292c0b52e02dd72",
+    measurementId: "G-9VKQV8DCHY"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB0FmBwwKtiIWpYGMcXwr7TUTBdcJKFkCE",
+    authDomain: "fitness-traker-d4210.firebaseapp.com",
+    databaseURL: "https://fitness-traker-d4210.firebaseio.com",
+    projectId: "fitness-traker-d4210",
+    storageBucket: "fitness-traker-d4210.appspot.com",
+    messagingSenderId: "108275168631",
+    appId: "1:108275168631:web:f4344a7292c0b52e02dd72",
+    measurementId: "G-9VKQV8DCHY"
+  }
+};
